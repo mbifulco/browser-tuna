19b1f2d0a00e9b7daceb2a6ac67827846836b463
Mike Bifulco <mbifulco@live.com>
2021-12-05 10:13:39 -0500

    microphonemanager class

--- components/Tuner/Tuner.js
+++ components/Tuner/Tuner.js
@@ -1,74 +1,113 @@
 import { useEffect, useRef, useState } from 'react';
-import AudioDevices from '../../lib/AudioDevices';
+import AudioInputManager from '../../lib/AudioInputManager';
+import MicrophoneManager from '../../lib/MicrophoneManager';
 
-import { Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import { Button, Radio, RadioGroup, Stack } from '@chakra-ui/react';
+
+const FFT_SIZE = 512;
 
 const Tuner = () => {
-  const [audioInterface, setAudioInterface] = useState();
-  const [audioInterfaceSelection, setAudioInterfaceSelection] = useState('0');
+  const [selectedDeviceId, setSelectedDeviceId] = useState();
 
-  const [count, setCount] = useState(0);
-  // Use useRef for mutable variables that we want to persist
-  // without triggering a re-render on their change
-  const requestRef = useRef();
-  const previousTimeRef = useRef();
+  const canvasRef = useRef();
+  const frequencyDataRef = useRef();
+  const timeDomainDataRef = useRef();
 
-  const animate = (time) => {
-    if (!audioInterface?.frequencyAnalyzer) return;
+  // store microphone manager in state
+  const [microphoneManager, setMicrophoneManager] = useState(
+    () => new MicrophoneManager(FFT_SIZE),
+  );
 
-    if (previousTimeRef.current != undefined) {
-      const deltaTime = time - previousTimeRef.current;
+  const width = 800;
+  const height = 200;
 
-      var bufferLength = audioInterface.frequencyAnalyzer.frequencyBinCount;
-      var dataArray = new Uint8Array(bufferLength);
-      audioInterface.frequencyAnalyzer.getFloatFrequencyData(dataArray);
+  const draw = (ctx) => {
+    const sampleCount = FFT_SIZE / 2;
 
-      // Pass on a function to the setter of the state
-      // to make sure we always have the latest state
-      setCount(dataArray);
+    const canvasW = canvasRef.current.width;
+    const canvasH = canvasRef.current.height;
+
+    const barWidth = (canvasW * 1.0) / sampleCount; // * 2.5;
+
+    ctx.clearRect(0, 0, canvasW, canvasH);
+
+    ctx.fillStyle = 'rgba(0,0,0,0.0)';
+    ctx.fillRect(0, 0, canvasW, canvasH);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = '#000000';
+    ctx.beginPath();
+
+    let x = 0;
+
+    console.log(timeDomainDataRef.current, 1);
+
+    for (let i = 0; i < sampleCount; i++) {
+      const barHeight = timeDomainDataRef.current[i] * canvasH;
+      ctx.fillStyle = '#ee60ac';
+      ctx.fillRect(x, canvasH - barHeight / 2, barWidth, barHeight / 2);
+
+      x += barWidth;
     }
-    previousTimeRef.current = time;
-    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  const monitorMicrophone = () => {
+    if (microphoneManager.initialized) {
+      frequencyDataRef.current = microphoneManager.getFrequencyData();
+      timeDomainDataRef.current = microphoneManager.getTimeDomainData();
+
+      const canvas = canvasRef.current;
+      const context = canvas.getContext('2d');
+
+      if (timeDomainDataRef.current) {
+        console.log('drawing');
+        draw(context);
+      }
+    }
+    requestAnimationFrame(monitorMicrophone);
   };
 
   useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []); // Make sure the effect runs only once
-
-  useEffect(() => {
-    async function initializeAudio() {
-      const ai = new AudioDevices();
-      await ai.init();
-      setAudioInterface(ai);
-    }
-    initializeAudio();
+    // set up and start listening
+    monitorMicrophone();
   }, []);
 
   return (
-    <section>
-      {count}
-      {audioInterface?.devices?.length && (
-        <RadioGroup
-          name="audio-interface-selection"
-          onChange={setAudioInterfaceSelection}
-          value={audioInterfaceSelection}
-        >
-          <Stack>
-            {audioInterface.devices.map((device, idx) => {
-              return (
-                <Radio
-                  value={idx + ''}
-                  key={`${device.kind}-${device.groupId}-${device.deviceId}`}
-                >
-                  {device.label}
-                </Radio>
-              );
-            })}
-          </Stack>
-        </RadioGroup>
-      )}
-    </section>
+    <Stack>
+      <canvas ref={canvasRef} width={width} height={height} />
+      {/* {frequencyData} */}
+
+      {/* {audioInputManager.devices?.length && (
+        <Stack>
+          <h1>Listening? {audioInputManager.isListening ? 'Yes' : 'No'}</h1>
+          <RadioGroup
+            name="audio-interface-selection"
+            onChange={setSelectedDeviceId}
+            value={selectedDeviceId}
+          >
+            <Stack>
+              {audioInputManager.devices.map((device) => {
+                return (
+                  <Radio
+                    value={device.deviceId}
+                    key={`${device.kind}-${device.groupId}-${device.deviceId}`}
+                  >
+                    {device.label}
+                  </Radio>
+                );
+              })}
+            </Stack>
+          </RadioGroup>
+
+          <Button
+            onClick={() => {
+              audioInputManager?.listenToDevice(selectedDeviceId);
+            }}
+          >
+            Start
+          </Button>
+        </Stack>
+      )} */}
+    </Stack>
   );
 };
 
